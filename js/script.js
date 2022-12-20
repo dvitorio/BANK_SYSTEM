f0358a5c88920b3ab5d5d486c7a52dbc4358383d
Daniel Vitorio <dvitorio2@gmail.com>
2022-12-19 22:30:25 -0300

    Implementação da tela de nova conta

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 //INICIALIZAÇÃO DE VARIÁVEIS
+const btnCancelNewAccount = document.getElementById('btnCancelNewAccount');
+const btnCreateNewAccount = document.getElementById('btnCreateNewAccount');
 const btnLogin = document.getElementById('btnLogin');
-const loginScreen = document.getElementById('loginScreen');
-const menuOptions = document.getElementById('menuOptions');
+const btnNewAccount = document.getElementById('btnNewAccount');
 const carouselExampleCaptions = document.getElementById('carouselExampleCaptions');
+const loginScreen = document.getElementById('loginScreen');
+const newAccount = document.getElementById('newAccount');
+const pageMenu = document.getElementById('pageMenu');
 
 
+
+//Realiza o login da aplicação
 btnLogin.addEventListener('click', ()=>{
      let login = document.getElementById('login');
      let password = document.getElementById('password');
-          if((login.value == "admin@admin.com") && (password.value == "12345678")){
+          if((login.value == "admin@admin.com") && (password.value == "123456")){
                loginScreen.classList.add('d-none');
-               menuOptions.classList.remove('d-none');
+               pageMenu.classList.remove('d-none');
                carouselExampleCaptions.classList.remove('d-none');
                alert('Bem-vindo ao BANK SYSTEM!');
           }
@@ -18,3 +24,24 @@ btnLogin.addEventListener('click', ()=>{
                alert('Login ou senha inválido(s)! Tente novamente!')
           }
 });
+
+//Abre modal de nova conta
+btnNewAccount.addEventListener('click', ()=>{
+     newAccount.classList.remove('d-none');
+     pageMenu.classList.add('d-none');
+     carouselExampleCaptions.classList.add('d-none');
+     let newAccountCliente = document.getElementById('newAccountCliente').value;
+     let newAccountNumber = parseInt(document.getElementById('newAccountNumber').value);
+     let newAccountDate = document.getElementById('newAccountDate').value;
+     let initialDepositAccount = parseFloat(document.getElementById('initialDepositAccount').value);
+});
+
+//Fecha o modal de nova conta
+btnCancelNewAccount.addEventListener('click', ()=>{
+     loginScreen.classList.add('d-none');
+     newAccount.classList.add('d-none');
+     pageMenu.classList.remove('d-none');
+     carouselExampleCaptions.classList.remove('d-none');
+});
+
+
